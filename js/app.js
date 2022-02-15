8f59b00ac5a436d3bf04e452f6c6d32c045cb61c
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-15 21:40:37 +0100

    ho fatto uno switch case che a seconda della option scelta invoca le funzioni e passa due parametri che diranno il numero di quadrati da generare

--- js/app.js
+++ js/app.js
@@ -7,45 +7,66 @@ console.log(difficultyLevel);
 //Salvo il bottone play in una costante
 const playButton = document.querySelector('.btn');
 console.log(playButton);
+
 //faccio una funzione che dato un numero ritorna un array con n DIV ed n Numeri
 function squaresGenerator(num){
-    // const numArray = [];
+
     let square
+    const root = Math.sqrt(num);
     for(let i = 1; i <= num; i++){
         square = document.createElement('div');
         square.classList.add('square');
         gridWrapper.append(square);
         square.innerHTML += i;
-        // numArray.push(i);
+        /*calcolo l'altezza e larghezza dei quadrati con calc e la radice quadrata del parametro
+        salvata nella costante root così da avere sempre le dimensioni corrette anche
+        all'aumentare di righe e colonne*/
+        square.style.width = `calc( 100% / ${root})`;
+        square.style.height = `calc( 100% / ${root})`;
     }
-
+    
     return square;
 }
+//funzione che dati come parametri righe e colonne, restituisce il numero di quadrati
+function squaresNumber(row,col){
 
+    let squaresNumber = row * col;
+    return squaresNumber;
+}
 
-let level;
 // let squareArray = [];
-let rows = 7;
-let columns = 7;
-let squaresNumber = rows * columns;
-console.log(`ci sono ${squaresNumber} quadrati`);
+// let rows = 0;
+// let columns = 0;
+// console.log(`ci sono ${squaresNumber} quadrati`);
+let level;
+let squares;
+
 
 playButton.addEventListener('click', function(){
-    // console.log('prova');
+
+    gridWrapper.innerHTML = ''; //tramite questo resetto il contenuto di grid-wrapper, così non si accumula
     level = difficultyLevel.value;
     // console.log(level);
     switch(level){
         case 'easy':
             console.log('hai scelto easy');
-
+            squares = squaresNumber(7,7);
+            console.log(squares);
+            console.log(squaresGenerator(squares));
             break;
         case 'medium':
             console.log('hai scelto medium');
+            squares = squaresNumber(9,9);
+            console.log(squares);
+            console.log(squaresGenerator(squares));
             break;
         case 'difficult':
             console.log('hai scelto difficult');
+            squares = squaresNumber(10,10);
+            console.log(squares);
+            console.log(squaresGenerator(squares));
             break;
     }
+
 })
 
-console.log(squaresGenerator(squaresNumber));
